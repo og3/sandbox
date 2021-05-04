e0cf4a91d53cb48d9d0c6768e9aac86471453f6e
noriyuki togashi <>
2021-05-04 16:55:54 +0900

    usersテーブルにログイン状態を保存するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210503222357) do
+ActiveRecord::Schema.define(version: 20210504074517) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                   default: "", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20210503222357) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "online"
+    t.datetime "online_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
