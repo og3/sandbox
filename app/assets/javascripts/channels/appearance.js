488a032fc2d79dce3a4d3a64dd588505d9b9ca05
noriyuki togashi <>
2021-05-05 12:44:20 +0900

    最終オンライン時間を表示できる

--- app/assets/javascripts/channels/appearance.js
+++ app/assets/javascripts/channels/appearance.js
@@ -10,7 +10,7 @@ App.appearance = App.cable.subscriptions.create({
     };
     if (user.online === false){
       var element = document.getElementById("user_" + user.id);
-        element.innerHTML =  user.name + "：オフライン(js)"
+        element.innerHTML =  user.name + "：オフライン(js)" + " 最終オンライン時間：" + user.online_at
     };
   }
 });
