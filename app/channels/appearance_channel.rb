2571fe9accdda52c05b4da5b06ed76ca36acd158
noriyuki togashi <>
2021-05-05 11:59:31 +0900

    リファクタリング（まだ不十分）

--- app/channels/appearance_channel.rb
+++ app/channels/appearance_channel.rb
@@ -1,24 +1,28 @@
 class AppearanceChannel < ApplicationCable::Channel
   # クライアントがサーバーに接続したと同時に実行されるメソッド
   def subscribed
-    member = User.find(current_user.id)
-    return unless member
-    before_member_online_status = member.online
-    member.update_attributes(online: true, online_at: DateTime.now)
-    if before_member_online_status != member.online
-      AppearanceBroadcastJob.perform_later(member)
-    end
+    set_member
+    return unless @member
+    broadcast_online_status(before_online_status: @member.online, after_online_status: true)
     stream_from "appearance_user"
   end
 
   # クライアントの接続が解除されたと同時に実行されるメソッド
   def unsubscribed
-    member = User.find(current_user.id)
-    return unless member
-    before_member_online_status = member.online
-    member.update_attributes(online: false, online_at: DateTime.now)
-    if before_member_online_status != member.online
-      AppearanceBroadcastJob.perform_later(member)
+    set_member
+    return unless @member
+    broadcast_online_status(before_online_status: @member.online, after_online_status: false)
+  end
+
+  private
+  def broadcast_online_status(before_online_status:, after_online_status:)
+    @member.update_attributes(online: after_online_status, online_at: DateTime.now)
+    if before_online_status != @member.online
+      AppearanceBroadcastJob.perform_later(@member)
     end
+  end
+
+  def set_member
+    @member = User.find(current_user.id)
   end
 end
