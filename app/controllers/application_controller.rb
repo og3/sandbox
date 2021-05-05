8c61f41ef6eb2c2a3abd35b17c571432af4afb69
noriyuki togashi <>
2021-05-05 12:23:48 +0900

    アカウント登録時と編集時にnameを保存できる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user_id_on_cookies
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def set_current_user_id_on_cookies
     cookies.encrypted[:user_id] ||= current_user.id
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
